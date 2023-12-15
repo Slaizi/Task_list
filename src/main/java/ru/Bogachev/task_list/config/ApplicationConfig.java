e16bc7622a206147bdca7f5ff9da028798b738c7
Slaizi <bogachev_konstantin@bk.ru>
2023-12-15 19:26:14 +0300

    Docker

--- src/main/java/ru/Bogachev/task_list/config/ApplicationConfig.java
+++ src/main/java/ru/Bogachev/task_list/config/ApplicationConfig.java
@@ -6,15 +6,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -71,16 +68,18 @@ public class ApplicationConfig {
                 .cors(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling(exception -> exception.authenticationEntryPoint(
-                        (request, response, authException) -> {
-                            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                            response.getWriter().write("Unauthorized.");
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(
+                            (request, response, authException) -> {
+                                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                                response.getWriter().write("Unauthorized.");
                         })
                         .accessDeniedHandler(
-                                (request, response, accessDeniedException) -> {
-                                    response.setStatus(HttpStatus.FORBIDDEN.value());
-                                    response.getWriter().write("Unauthorized.");
-                                }))
+                            (request, response, accessDeniedException) -> {
+                                response.setStatus(HttpStatus.FORBIDDEN.value());
+                                response.getWriter().write("Unauthorized.");
+                        })
+                )
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
